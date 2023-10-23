f0e45738678046422047bf5a5d067216c0530fa4
IbroElhadi <148716797+IbroElhadi@users.noreply.github.com>
2023-10-23 09:41:52 -0400

    Update Assignment2.java

--- Assignment2.java
+++ Assignment2.java
@@ -1,10 +1,10 @@
 public class Assignment2 {// declare class
-       /**
- * Assignment2 - Main Class
- * Ibrahim
- * Date: 2023-10-22
- * This program allows users to run menu
- */
+ //*
+ //* Assignment2 - Main Class
+ //* Ibrahim
+// * Date: 2023-10-22
+// * This program allows users to run menu
+ //*
     public static void main(String[] args) { // main method
         Menu menu = new Menu(); // new object of menu in menu
 
